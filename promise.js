f8dbcf50869b685e06953a665876b9caf8a49581
rangdayy <choazranda@gmail.com>
2022-09-16 13:33:12 +0800

    panggil fungsi promise

--- promise.js
+++ promise.js
@@ -1,8 +1,25 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
-
+const promiseOutput = (emosi) => {
+  let count = 0;
+  promiseTheaterIXX()
+    .then((result) => {
+      result.map((values) => {
+        if (values.hasil == emosi) count += 1;
+      });
+      return promiseTheaterVGC();
+    })
+    .then((result) => {
+      result.map((values) => {
+        if (values.hasil == emosi) count += 1;
+      });
+      console.log(count)
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
 module.exports = {
   promiseOutput,
 };
